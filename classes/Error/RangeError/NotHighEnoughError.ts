2597cfd1c9c1c27f68bed21da974f2ffd8614044
Luca <klatu1494@gmail.com>
2019-03-31 22:39:05 -0300

    NotHighEnoughError now uses the DOT constant.

--- classes/Error/RangeError/NotHighEnoughError.ts
+++ classes/Error/RangeError/NotHighEnoughError.ts
@@ -1,6 +1,6 @@
 class NotHighEnoughError extends RangeError {
     private static readonly hint: string = " must be higher than ";
-    private static readonly endingDot: string = ".";
+    private static readonly endingDot: string = DOT;
 
     public constructor(entity: string, minimumValue: number) {
         super(
