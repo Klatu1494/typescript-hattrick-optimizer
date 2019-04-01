2e8cd640ee471c94f6716dcfce53e58f87a444c0
Luca <klatu1494@gmail.com>
2019-04-01 01:10:22 -0300

    NotHighEnoughError was modified.
    NotHighEnoughError now considers that its parameters can be equal.

--- classes/Error/RangeError/NotHighEnoughError.ts
+++ classes/Error/RangeError/NotHighEnoughError.ts
@@ -1,11 +1,20 @@
 class NotHighEnoughError extends RangeError {
-    private static readonly hint: string = " must be higher than ";
+    private static readonly disallowedEqualityHint: string =
+        " must be higher than ";
+    private static readonly allowedEqualityHint: string =
+        " must be higher than or equal to ";
     private static readonly endingDot: string = DOT;
 
-    public constructor(entity: string, minimumValue: string) {
+    public constructor(
+        entity: string,
+        minimumValue: string,
+        allowEquality: boolean
+    ) {
         super(
             entity +
-                NotHighEnoughError.hint +
+                (allowEquality
+                    ? NotHighEnoughError.allowedEqualityHint
+                    : NotHighEnoughError.disallowedEqualityHint) +
                 minimumValue +
                 NotHighEnoughError.endingDot
         );
