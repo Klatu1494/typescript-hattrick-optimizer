e957d22df03938fbab0c15e46e509d2c85803894
Luca <klatu1494@gmail.com>
2019-04-02 14:25:31 -0300

    The formations are now generated in the index.ts.

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 addEventListener("load", () => {
-    let roles: ReadonlyArray<Role> = [
+    let formations: ReadonlyArray<Formation> = [
         CentralDefender.getInstance(),
         CentralDefenderTowardsLateral.getInstance(),
         DefensiveLateralDefender.getInstance(),
@@ -19,11 +19,66 @@ addEventListener("load", () => {
         StrikerTowardsLateral.getInstance(),
         Wing.getInstance(),
         WingTowardsMiddle.getInstance()
-    ];
-    let rolesCombinations: ReadonlyArray<
-        ReadonlyArray<Role>
-    > = Permutator.getInstance().getPermutations(
-        roles,
-        AMOUNT_OF_PLAYERS_IN_A_FORMATION
-    );
+    ]
+        .reduce<ReadonlyArray<Map<Role, number>>>(
+            (
+                previousArray: ReadonlyArray<Map<Role, number>>,
+                role: Role,
+                index: number,
+                roles: ReadonlyArray<Role>
+            ): ReadonlyArray<Map<Role, number>> => {
+                let newArray: Array<Map<Role, number>> = [];
+                let zone: IZone = role.getZone();
+                let playersFulfillingRoleMaxAmount: number = role.getPossibleNearestSides()
+                    .length;
+                let maximumPossibleRemainingPlayers: number = 0;
+                let rolesAmount: number = roles.length;
+                for (let i: number = index + 1; i < rolesAmount; i++) {
+                    maximumPossibleRemainingPlayers += roles[
+                        i
+                    ].getPossibleNearestSides().length;
+                }
+                for (let currentMap of previousArray) {
+                    let playersInMapAmount: number = 0;
+                    let playersInZoneAmount: number = 0;
+                    let i: number = 0;
+                    for (let playersFulfillingRoleAmount of currentMap) {
+                        playersInMapAmount += playersFulfillingRoleAmount[1];
+                        playersInZoneAmount +=
+                            playersFulfillingRoleAmount[0].getZone() === zone
+                                ? playersFulfillingRoleAmount[1]
+                                : 0;
+                    }
+                    while (
+                        i <=
+                            playersFulfillingRoleMaxAmount -
+                                playersInZoneAmount &&
+                        playersInMapAmount + i <=
+                            AMOUNT_OF_PLAYERS_IN_A_FORMATION &&
+                        AMOUNT_OF_PLAYERS_IN_A_FORMATION <=
+                            playersInMapAmount + maximumPossibleRemainingPlayers
+                    ) {
+                        let currentMapCopy: Map<Role, number> = new Map(
+                            currentMap
+                        );
+                        currentMapCopy.set(role, i);
+                        newArray.push(currentMapCopy);
+                        i++;
+                    }
+                }
+                return newArray;
+            },
+            [new Map()]
+        )
+        .map<Formation>(
+            (element: Map<Role, number>): Formation => new Formation(element)
+        )
+        .filter(
+            (
+                element: Formation,
+                _index: number,
+                _array: ReadonlyArray<Formation>
+            ) => element.isValid()
+        );
+    console.log(formations);
 });
