41b08d4793880c8f829e050e04e3f0197011292d
Luca <klatu1494@gmail.com>
2019-04-02 01:08:04 -0300

    HattrickOptimizerProblem was updated.
    The contributions expressions were created.
    The expectedMidfield parameter was removed since it's not necessary.

--- classes/HattrickOptimizerProblem.ts
+++ classes/HattrickOptimizerProblem.ts
@@ -6,24 +6,27 @@ class HattrickOptimizerProblem {
         private readonly expectedLeftDefense: number,
         private readonly expectedCentralDefense: number,
         private readonly expectedRightDefense: number,
-        private readonly expectedMidfield: number,
         private readonly expectedLeftAttack: number,
         private readonly expectedCentralAttack: number,
         private readonly expectedRightAttack: number
     ) {}
 
     public getSolution(): HattrickOptimizerSolution {
-        let amountOfPlayers: number = this.players.length;
-        let leftDefenseExpression: IDifferentiableExpression;
-        let centralDefenseExpression: IDifferentiableExpression;
-        let rightDefenseExpression: IDifferentiableExpression;
-        let midfieldExpression: IDifferentiableExpression;
-        let leftAttackExpression: IDifferentiableExpression;
-        let centralAttackExpression: IDifferentiableExpression;
-        let rightAttackExpression: IDifferentiableExpression;
-        let firstPlayer: Player;
-        let playersUses: Array<PlayerVariableExpression>;
-        if (amountOfPlayers <= 10) {
+        function getValueTerm(
+            playerVariable: PlayerVariableExpression,
+            playerValue: number,
+            oppositeOpponentValue: number
+        ): IDifferentiableExpression {
+            return new DifferentiableQuotient(
+                new DifferentiableProduct(
+                    new ConstantExpression(oppositeOpponentValue + playerValue),
+                    playerVariable
+                ),
+                new ConstantExpression(this.expectedRightAttack)
+            );
+        }
+
+        if (this.players.length <= 10) {
             throw new NotHighEnoughError(
                 AMOUNT_OF_PLAYERS_DESCRIPTION,
                 (10).toString(),
@@ -46,12 +49,93 @@ class HattrickOptimizerProblem {
                         let zoneFilledPositions: Set<
                             NearestSide
                         > = filledPositions.get(zone);
+                        let leftDefenseExpression: IDifferentiableExpression = new ConstantExpression(
+                            0
+                        );
+                        let centralDefenseExpression: IDifferentiableExpression = new ConstantExpression(
+                            0
+                        );
+                        let rightDefenseExpression: IDifferentiableExpression = new ConstantExpression(
+                            0
+                        );
+                        let midfieldExpression: IDifferentiableExpression = new ConstantExpression(
+                            0
+                        );
+                        let leftAttackExpression: IDifferentiableExpression = new ConstantExpression(
+                            0
+                        );
+                        let centralAttackExpression: IDifferentiableExpression = new ConstantExpression(
+                            0
+                        );
+                        let rightAttackExpression: IDifferentiableExpression = new ConstantExpression(
+                            0
+                        );
+                        let playersUses: Array<PlayerVariableExpression>;
                         zoneFilledPositions.add(nearestSide);
-                        //TODO: initialize expressions
-                        for (let i: number = 1; i < amountOfPlayers; i++) {
-                            let currentPlayer: Player = this.players[i];
+                        for (let currentPlayer of this.players) {
+                            let currentPlayerPerformance: PlayerPerformance = currentPlayer.getPerformance(
+                                role,
+                                nearestSide
+                            );
                             let currentPlayerVariable: PlayerVariableExpression = new PlayerVariableExpression(
                                 currentPlayer
+                            );
+                            leftDefenseExpression = new DifferentiableSum(
+                                leftDefenseExpression,
+                                getValueTerm(
+                                    currentPlayerVariable,
+                                    currentPlayerPerformance.getLeftDefenseContribution(),
+                                    this.expectedRightAttack
+                                )
+                            );
+                            centralDefenseExpression = new DifferentiableSum(
+                                centralDefenseExpression,
+                                getValueTerm(
+                                    currentPlayerVariable,
+                                    currentPlayerPerformance.getCentralDefenseContribution(),
+                                    this.expectedCentralAttack
+                                )
+                            );
+                            rightDefenseExpression = new DifferentiableSum(
+                                rightDefenseExpression,
+                                getValueTerm(
+                                    currentPlayerVariable,
+                                    currentPlayerPerformance.getRightDefenseContribution(),
+                                    this.expectedLeftAttack
+                                )
+                            );
+                            midfieldExpression = new DifferentiableSum(
+                                midfieldExpression,
+                                new DifferentiableProduct(
+                                    new ConstantExpression(
+                                        currentPlayerPerformance.getMidfieldContribution()
+                                    ),
+                                    currentPlayerVariable
+                                )
+                            );
+                            leftAttackExpression = new DifferentiableSum(
+                                leftAttackExpression,
+                                getValueTerm(
+                                    currentPlayerVariable,
+                                    currentPlayerPerformance.getLeftAttackContribution(),
+                                    this.expectedRightDefense
+                                )
+                            );
+                            centralAttackExpression = new DifferentiableSum(
+                                centralAttackExpression,
+                                getValueTerm(
+                                    currentPlayerVariable,
+                                    currentPlayerPerformance.getCentralAttackContribution(),
+                                    this.expectedCentralDefense
+                                )
+                            );
+                            rightAttackExpression = new DifferentiableSum(
+                                rightAttackExpression,
+                                getValueTerm(
+                                    currentPlayerVariable,
+                                    currentPlayerPerformance.getRightAttackContribution(),
+                                    this.expectedLeftDefense
+                                )
                             );
                         }
                         zoneFilledPositions.delete(nearestSide);
